936d7cc4a3011533babafabbe88e5611471eb8eb
Patrick José Pereira <patrickelectric@gmail.com>
2017-11-09 21:45:52 -0200

    video: update version to work with threads
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/user/video.py
+++ src/user/video.py
@@ -1,29 +1,57 @@
-import cv2
-import numpy as np
+#!/usr/bin/env python
+"""
+BlueRov video capture class
+"""
 import threading
 
+import cv2
 
-class Video(object):
+class Video(threading.Thread):
+
+    """BlueRov video capture class constructor
+
+    Attributes:
+        cap (VideoCapture): Camera capture object
+        ip (string): Video udp ip address
+        port (int): Video udp port
+    """
+
     def __init__(self, ip='127.0.0.1', port=5600):
+        super(Video, self).__init__()
+
         self.ip = ip
         self.port = port
         self._frame = None
 
-    def start_capture(self):
-        'Start video capture'
         self.cap = cv2.VideoCapture('udp://{0}:{1}'.format(self.ip, self.port))
-        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
-        self.frame(True)
 
-    def frame(self, fake=False):
-        'Return frame'
-        if fake:
-            # Update frame buffer
-            self._frame = self.cap.read()
-            thread = threading.Timer(1 / self.fps, self.frame, [True])
-            thread.daemon = True
-            thread.start()
+    def frame(self):
+        """ Get Frame
+
+        Returns:
+            iterable: bool and image frame, cap.read() output
+        """
         return self._frame
+
+    def frame_available(self):
+        """Check if frame is available
+
+        Returns:
+            bool: true if frame is available
+        """
+        return self._frame != None
+
+    def run(self):
+        """ Get frame to update _frame
+        """
+        try:
+            new_frame = self.cap.read()
+            if new_frame != None:
+                self._frame = new_frame
+
+        except Exception as error:
+            print(error)
+        self.run()
 
     def __exit__(self, exc_type, exc_value, traceback):
         'Release camera'
@@ -31,12 +59,14 @@ class Video(object):
 
 
 if __name__ == '__main__':
-    vid = Video()
-    vid.start_capture()
+    video = Video()
+    video.start()
 
-    while(True):
-        ret, frame = vid.frame()
+    while not video.frame_available():
+        pass
+
+    while True:
+        ret, frame = video.frame()
         cv2.imshow('frame', frame)
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break

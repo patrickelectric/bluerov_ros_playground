cc456256352fe38896c0802790876e9a2a326b63
Patrick José Pereira <patrickelectric@gmail.com>
2017-11-10 23:41:56 -0200

    user: Update dosctring
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/user/user.py
+++ src/user/user.py
@@ -18,6 +18,15 @@ from sensor_msgs.msg import JointState
 
 
 class Code(threading.Thread):
+
+    """Class to provide user access
+
+    Attributes:
+        cam (Video): Video object, get video stream
+        pub (Pub): Pub object, do topics publication
+        sub (Sub): Sub object, subscribe in topics
+    """
+
     def __init__(self):
         super(Code, self).__init__()
         self.sub = subs.Subs()
@@ -35,8 +44,16 @@ class Code(threading.Thread):
             pass
 
     def pwm_to_thrust(self, pwm):
-        # Equation from
-        #    https://colab.research.google.com/notebook#fileId=1CEDW9ONTJ8Aik-HVsqck8Y_EcHYLg0zK
+        """Transform pwm to thruster value
+        The equation come from:
+            https://colab.research.google.com/notebook#fileId=1CEDW9ONTJ8Aik-HVsqck8Y_EcHYLg0zK
+
+        Args:
+            pwm (int): pwm value
+
+        Returns:
+            float: Thrust value
+        """
         return -3.04338931856672e-13*pwm**5 \
             + 2.27813523978448e-9*pwm**4 \
             - 6.73710647138884e-6*pwm**3 \
@@ -46,6 +63,8 @@ class Code(threading.Thread):
 
 
     def run(self):
+        """Run user code
+        """
         while True:
             # Try to get data
             try:
@@ -56,15 +75,19 @@ class Code(threading.Thread):
                 print('Get data error:', error)
 
             try:
+                # Get joystick data
                 joy = self.sub.get_data()['joy']['axes']
 
-                # pwm run between 1100 and 2000, a joy command is between -1.0 and 1.0
+                # rc run between 1100 and 2000, a joy command is between -1.0 and 1.0
                 override = [int(val*400 + 1500) for val in joy]
+                # Send joystick data as rc output into rc override topic
+                # (fake radio controller)
                 self.pub.set_data('/mavros/rc/override', override)
             except Exception as error:
                 print('joy error:', error)
 
             try:
+                # Get pwm output and send it to Gazebo model
                 rc = self.sub.get_data()['mavros']['rc']['out']['channels']
                 joint = JointState()
                 joint.name = ["thr{}".format(u + 1) for u in range(5)]
@@ -75,6 +98,7 @@ class Code(threading.Thread):
                 print('rc error:', error)
 
             try:
+                # Show video output
                 _, frame = self.cam.frame()
                 cv2.imshow('frame', frame)
                 cv2.waitKey(1)
